cf101ab22eb146ce36f77395c20a6eb31264f7c2
KsmLeopard09 <mangatkuljyot95@gmail.com>
2024-04-15 11:59:19 -0600

    Made enemies start shooting towards player

--- Assets/Scripts/PurpleBotProjectile.cs
+++ Assets/Scripts/PurpleBotProjectile.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
 using UnityEngine;
 
 public class PurpleBotProjectile : MonoBehaviour
@@ -10,19 +11,26 @@ public class PurpleBotProjectile : MonoBehaviour
     private Vector2 target;
 
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        // Calculate the direction from the bullet to the player
+        Vector3 direction = player.position - transform.position;
 
-        target = new Vector2(player.position.x, player.position.y);
+        // Project the direction onto the x-y plane to remove the z component
+        direction.z = 0;
+
+        // Rotate the bullet to face the player on the z axis only
+        transform.right = direction.normalized;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        transform.position += transform.right * speed * Time.deltaTime;
 
-        if(transform.position.x == target.x && transform.position.y == target.y)
+        if (transform.position.x == target.x && transform.position.y == target.y)
         {
             DestroyProjectile();
         }
