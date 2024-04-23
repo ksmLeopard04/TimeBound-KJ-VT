ffa6c77f6239287aca910a7f1065b582571d0aab
jenriquez6 <victor.admapp01@gmail.com>
2024-04-23 12:35:34 -0600

    cacodaemon effect

---
+++ Assets/Cacodaemon.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cacodaemon : MonoBehaviour
+{
+    private Transform player;
+    private float normalSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        normalSpeed = player.GetComponent<PlayerController>().moveSpeed;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            player.GetComponent<PlayerController>().moveSpeed = 0;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        player.GetComponent<PlayerController>().moveSpeed = normalSpeed;
+    }
+}
