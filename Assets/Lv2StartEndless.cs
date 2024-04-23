09cd705d1f354515d9c68a1b5ccb37dc42bb99f0
jenriquez6 <victor.admapp01@gmail.com>
2024-04-23 16:27:55 -0600

    endless level (audio + some logic changes + edited tilemap lil bit)

--- Assets/Lv2StartEndless.cs
+++ Assets/Lv2StartEndless.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Lv2StartEndless : MonoBehaviour
@@ -9,6 +10,7 @@ public class Lv2StartEndless : MonoBehaviour
 
     [SerializeField]
     public GameObject spawner;
+
 
     // Start is called before the first frame update
     void Start()
@@ -26,8 +28,15 @@ public class Lv2StartEndless : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
+            
             boundary.SetActive(true);
             spawner.SetActive(true);
+
+            GameObject.Find("audio").transform.GetChild(0).gameObject.SetActive(false);
+            GameObject.Find("audio").transform.GetChild(1).gameObject.GetComponent<AudioSource>().Play();
+
+            gameObject.SetActive(false);
         }
     }
+
 }
